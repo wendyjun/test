a16223f98d96f44a9ff5caae947222d709223fe7
wendyjun <328971161@qq.com>
2016-07-25 17:39:33 +0800

    Update crontab_start.py

--- crontab_start.py
+++ crontab_start.py
@@ -2,6 +2,7 @@ import os
 
 if __name__ == "__main__":
 	print '========================='
+	#绝对路径
 	os.chdir("/home/gxj/Downloads/oppo/oppo")
 	os.system("/usr/local/bin/scrapy crawl oppo")
 	print 'the end'
